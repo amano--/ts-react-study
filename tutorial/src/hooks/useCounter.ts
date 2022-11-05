d0542b324e745cef91d768ab804ee3fcf8cceed4
amano-- <amapetas@gmail.com>
2022-11-05 20:44:57 +0900

    １

--- tutorial/src/hooks/useCounter.ts
+++ tutorial/src/hooks/useCounter.ts
@@ -1,15 +1,15 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 type useCounter = {
-  count: number
-  increment: () => void
-  decrement: () => void
-}
+  count: number;
+  increment: () => void;
+  decrement: () => void;
+};
 
 export default function useCounter(): useCounter {
-  const [count, setCount] = useState(0)
-  const increment = () => setCount((count) => count + 1)
-  const decrement = () => setCount((count) =>  count - 1 )
+  const [count, setCount] = useState(0);
+  const increment = () => setCount((count) => count + 1);
+  const decrement = () => setCount((count) => count - 1);
 
-  return { count, increment, decrement }
+  return { count, increment, decrement };
 }
