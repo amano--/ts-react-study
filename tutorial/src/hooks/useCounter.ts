94a99a0061435926617afc0264a837faeafda180
amano-- <amapetas@gmail.com>
2022-11-08 22:04:17 +0900

    1

--- tutorial/src/hooks/useCounter.ts
+++ tutorial/src/hooks/useCounter.ts
@@ -8,6 +8,7 @@ type useCounter = {
 
 export default function useCounter(): useCounter {
   const [count, setCount] = useState(0);
+  
   const increment = () => setCount((count) => count + 1);
   const decrement = () => setCount((count) => count - 1);
 
